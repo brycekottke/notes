dc5019cf502d293f5ded36d1a713caa3f7c6bf07
Bryce Kottke <brycek@mindtouch.com>
2020-01-29 20:17:13 -0500

    fixed update-zoom-check script so it posts to system tray

--- scripts/zoom/update-zoom-check.sh
+++ scripts/zoom/update-zoom-check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#NOTE: Need this to post to system tray with notify-send
+export XDG_RUNTIME_DIR="/run/user/1000"
+
 if ping -q -c 1 -W 1 4.2.2.2 > /dev/null; then
 
   wget --quiet -P /tmp/ -O /tmp/zoom-output https://zoom.us/download
@@ -16,7 +19,7 @@ if ping -q -c 1 -W 1 4.2.2.2 > /dev/null; then
   
   if [[ ${current_version} -lt ${latest_version} ]]; then
     echo "UPDATE ZOOM"
-    notify-send -u critical -t '12000' '!!! UPDATE ZOOM !!!'
+    /usr/bin/notify-send -u critical -t '12000' '!!! UPDATE ZOOM !!!'
   else
     echo "ZOOM IS UP TO DATE"
   fi
